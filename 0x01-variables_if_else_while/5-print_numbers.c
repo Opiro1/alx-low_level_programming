766809b2d227b44889478bebfb69241949d15bee
Opiro1 <johnsonopeoluwa2304@gmail.com>
2022-09-11 10:54:53 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: opiro
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int opiro;
+
+	for (opiro = 0; opiro < 10; opiro++)
+	{
+		printf("%d", opiro);
+	}
+	printf("\n");
+	return (0);
+}
