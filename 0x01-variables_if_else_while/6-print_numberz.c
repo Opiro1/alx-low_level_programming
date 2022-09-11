178100ce03f84faf40d1802c5acb2947a3050ef6
Opiro1 <johnsonopeoluwa2304@gmail.com>
2022-09-11 11:00:52 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: opiro
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int opiro;
+
+	for (opiro = 48; opiro < 58; opiro++)
+		putchar(opiro);
+	putchar('\n');
+	return (0);
+}
