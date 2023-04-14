601fde9a86cd6cb3cc2b34ff3127ba63afcc914d
Opiro1 <johnsonopeoluwa2304@gmail.com>
2023-04-14 01:49:29 -0700

    The search algorithm project

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -68,4 +68,26 @@ listint_t *create_list(int *array, size_t size);
 /* free_list - Deallocates a singly linked list */
 void free_list(listint_t *list);
 
+int advanced_binary(int *array, size_t size, int value);
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif /* SEARCH_H */
